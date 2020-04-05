bfb35edf2eeb07ff36d5ea403442794e015e7915
Maxou <Maxou@Pc-Bureau-Max>
2020-04-05 20:23:32 +0200

    Test 2

--- ProjetExamenJ/src/projetExamenPackage/Principale.java
+++ ProjetExamenJ/src/projetExamenPackage/Principale.java
@@ -1,10 +1,25 @@
 package projetExamenPackage;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import projetExamenPackage.AccessBDGen; 
+
 public class Principale {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
+		  
+		try {
+			Connection connection = AccessBDGen.connecter("DbInstallations","root", "Tigrou007"); // Etablir la connexion ("le câble qui relie le programme Java à la BD")  
+			String sqlInstruction = "insert into FamilleSoftware (IdFamSoft, Libelle)values (?,?)"; 
+			PreparedStatement myPrepStat = connection.prepareStatement(sqlInstruction); // Créer le PreparedStatement à partir de cette instruction SQL ("chariotsur câble")
+			myPrepStat.setInt(1,202); // remplacer les ? par valeurs introduites par user (pour éviter lesinjections SQL) 
+			myPrepStat.setString(2,"Ma famille Software "); // remplacer les ? par valeurs introduites par user (pour éviter lesinjections SQL) 
+			int nbUpdatedLines = myPrepStat.executeUpdate(); // Exécuter ("envoyer le chariot à la BD et demander d'exécuter l'instruction") 
+			System.out.println("Nombre de lignes modifiées: " + nbUpdatedLines); // Récupérer le nombre de lignes modifiées et l'afficher		
+		}
+		catch (SQLException e) {
+			System.out.println(e.getMessage()); }
+			}
 }
+		
