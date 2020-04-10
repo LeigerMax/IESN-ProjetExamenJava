dcfb52885f5343472a9b841c3d89bcbb0666456a
Maxou <Maxou@Pc-Bureau-Max>
2020-04-10 15:00:49 +0200

    A propos

--- ProjetExamenJ/src/projetExamenPackage/APropos.java
+++ ProjetExamenJ/src/projetExamenPackage/APropos.java
@@ -7,7 +7,6 @@ import javax.swing.*;
 
 public class APropos extends JFrame {
 
-	private Container cont;
 	private JLabel labelTexte1,labelTexte2,labelTexte3,labelTexte4;
 	private JButton boutonRetour;
 	
@@ -15,7 +14,6 @@ public class APropos extends JFrame {
 	public APropos() {
         super("A propos");
         setBounds(500,200,400,200);
-        cont = getContentPane();
         
 		labelTexte1 = new JLabel("Programme créer en Java sur Eclipse");
 		labelTexte1.setBounds(80, 20, 255, 45);
@@ -24,23 +22,22 @@ public class APropos extends JFrame {
         
         labelTexte2 = new JLabel("Fait par Maxime & Paul "); 
         labelTexte2.setBounds(80, 40, 255, 45);
-        cont.add(labelTexte2);
+        add(labelTexte2);
         
         labelTexte3 = new JLabel("Pour le cours Développement orienté objet avancé "); 
         labelTexte3.setBounds(80, 60, 300, 45);
-        cont.add(labelTexte3);
+        add(labelTexte3);
         
         labelTexte4 = new JLabel("  "); 
         labelTexte4.setBounds(80, 60, 255, 45);
-        cont.add(labelTexte4);
-
+        add(labelTexte4);
 
         
         boutonRetour = new JButton("Retour au menu principal");     
         boutonRetour.setBounds(200,400,100,30);    
         MonGestionnaireActionRetour BRetour = new MonGestionnaireActionRetour();     
         boutonRetour.addActionListener(BRetour); 
-        cont.add(boutonRetour,BorderLayout.PAGE_END); 
+        add(boutonRetour,BorderLayout.PAGE_END); 
  
         setVisible(true); }
 	
