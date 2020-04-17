323a75a5aabf7af243b50c16b213794b7fd22882
Maxou <Maxou@Pc-Bureau-Max>
2020-04-17 20:00:32 +0200

    Add SoftwaresAcquisDate

--- ProjetExamenJ/src/projetExamenPackage/SoftwaresAcquisDate.java
+++ ProjetExamenJ/src/projetExamenPackage/SoftwaresAcquisDate.java
@@ -10,8 +10,12 @@ public class SoftwaresAcquisDate extends JPanel {
 	private JLabel labelTitre;
 	private JButton boutonChoix;
 	private Fenetre parent;
-	private String SqlSelectFrom;
-	private JComboBox<String> comboxChoix;
+	private String SqlSelectFrom, stringDateDébut,stringDateFin;
+	private JComboBox<Integer> année, mois ,jour;
+	private JComboBox<Integer> annéeF, moisF ,jourF;
+
+
+
 	
 	public  SoftwaresAcquisDate(Connection connection, Fenetre fenetre) {
 		parent = fenetre;
@@ -20,30 +24,68 @@ public class SoftwaresAcquisDate extends JPanel {
 		labelTitre.setFont(new java.awt.Font(Font.SERIF,Font.BOLD,25));
 		add(labelTitre); 
 		
-		comboxChoix = new JComboBox<String>();
-		add(comboxChoix);
-		
 		boutonChoix = new JButton("Afficher");
 		add(boutonChoix);
 		
-		RécupérerNomsTableau(connection);
+
+		ActionAfficher a = new ActionAfficher();
+		boutonChoix.addActionListener(a);
+		
+		
+		année = new JComboBox<Integer>();
+		mois = new JComboBox<Integer>();
+		jour = new JComboBox<Integer>();
+		annéeF = new JComboBox<Integer>();
+		moisF = new JComboBox<Integer>();
+		jourF = new JComboBox<Integer>();
+
+		for(int annéeI=2020;annéeI>=1900;annéeI--) {
+			année.addItem(annéeI);
+			annéeF.addItem(annéeI);
+		}
+		for(int moisI=1;moisI<=12;moisI++) {
+			mois.addItem(moisI);
+			moisF.addItem(moisI);
+		}
+		for(int jourI=1;jourI<=31;jourI++) {
+			jour.addItem(jourI);
+			jourF.addItem(jourI);
+		}
+		
+		add(année);
+		add(mois);
+		add(jour);
+		add(annéeF);
+		add(moisF);
+		add(jourF);
+		
 		
 
 		setVisible(true);
 	}
 	
-	private void RécupérerNomsTableau(Connection connect) {
-		try {
-			PreparedStatement prepStat = connect.prepareStatement("select * from software; ");
-			TableModelGen table2 = AccessBDGen.creerTableModel(prepStat);
-			for(int i=0; i <= table2.getRowCount()-1; i++) {
-				comboxChoix.addItem((String) table2.getValueAt(i, 0));
-				}
-			}
-		catch(SQLException e) {
-			JOptionPane.showMessageDialog(null,e.getMessage(), "Erreur", JOptionPane.ERROR_MESSAGE);
-		}
-	}
-	
 
+	
+	
+	private class ActionAfficher implements ActionListener{
+		public void actionPerformed( ActionEvent a){
+			stringDateDébut = année.getSelectedItem()+"-"+(mois.getSelectedItem())+"-"+jour.getSelectedItem();
+			stringDateFin = annéeF.getSelectedItem()+"-"+(moisF.getSelectedItem())+"-"+jourF.getSelectedItem();
+			SqlSelectFrom = "SELECT * FROM software WHERE software.DateAcquisition BETWEEN '"+stringDateDébut+"' AND '"+stringDateFin+"';";
+			System.out.println(SqlSelectFrom);
+			AfficherUneTable afficherLaTable  = new AfficherUneTable(parent.getConnect(), SqlSelectFrom);
+
+			
+			removeAll();
+			add(labelTitre);
+			add(boutonChoix);
+			add(année);
+			add(mois);
+			add(jour);
+			add(annéeF);
+			add(moisF);
+			add(jourF);
+			add(afficherLaTable,BorderLayout.CENTER);
+			validate();
+		}}
 }
