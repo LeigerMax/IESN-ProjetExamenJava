13d6906428b2a79413e9562ba0cb2f2048798293
PaulApg <63246672+PaulApg@users.noreply.github.com>
2020-04-18 15:49:12 +0200

    Update Suppression.java

--- ProjetExamenJ/src/projetExamenPackage/Suppression.java
+++ ProjetExamenJ/src/projetExamenPackage/Suppression.java
@@ -9,7 +9,7 @@ import accessBD.*;
 public class Suppression extends JPanel {
 	private JLabel labelTitre, labelSelection;
 	private JButton supInstall;
-	private JComboBox<String> installExist;
+	private JComboBox<String> installExist, fammilleSoft;
 	private Fenetre parent;
 	private String SqlSelectFrom;
 	
@@ -22,10 +22,16 @@ public class Suppression extends JPanel {
 		labelTitre.setBounds(245,10,450,30); 
 		add(labelTitre); 
 		
-		labelSelection = new JLabel("Sélectionnez la ligne à supprimer");
-		labelSelection.setFont(new java.awt.Font(Font.SERIF,Font.ITALIC,15));
-		labelSelection.setBounds(250, 60, 300, 30);
-		add(labelSelection);
+		labelFamilleSoft = new JLabel("Famille de Software : ");
+		labelFamilleSoft.setHorizontalAlignment(SwingConstants.LEFT);
+		labelFamilleSoft.setBounds(150, 60, 300, 30);
+		add(labelFamilleSoft);
+		String[ ] contSoft = {"Windows", "Linux", "Mac IOS"};
+		familleSoft = new JComboBox(contSoft);
+		familleSoft.setBounds(280, 60, 150, 30);
+		familleSoft.setSelectedItem("");
+		familleSoft.setMaximumRowCount(3);
+		add(familleSoft);
 		
 		supInstall = new JButton("Supprimer"); 
 		supInstall.setBounds(100,400,120,40); 
