18e71f4689750c274ad96616a3b2556633205431
Maxou <Maxou@Pc-Bureau-Max>
2020-04-19 21:42:11 +0200

    add ComboxDate

---
+++ ProjetExamenJ/src/projetExamenPackage/ComboxDate.java
@@ -0,0 +1,49 @@
+package projetExamenPackage;
+
+import java.awt.*;
+import java.awt.event.*;
+import java.sql.*;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import javax.swing.*;
+import accessBD.*;
+
+public class ComboxDate extends JPanel {
+	private JComboBox<Integer> année,jour;
+	private JComboBox<String>  mois;
+	private String[] listeMois = {"Janvier","Février","Mars","Avril","Mai","Juin","Juillet","Août","Septembre","Octobre","Novembre","Décembre"};
+	private java.util.Date dateJava;
+	
+	public ComboxDate() {
+		setLayout(new GridLayout(1,3,1,1));
+		
+		année = new JComboBox<Integer>();
+		mois = new JComboBox<String>(listeMois);
+		jour = new JComboBox<Integer>();
+		for(int annéeI=2025;annéeI>=2000;annéeI--) {
+			année.addItem(annéeI);
+		}
+		for(int jourI=1;jourI<=31;jourI++) {
+			jour.addItem(jourI);
+		}
+		
+		add(année);
+		add(mois);
+		add(jour);
+		
+		setVisible(true);
+		}
+		
+		public Date getDate() {
+			SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMdd");
+			String dateText = année.getSelectedItem()+"-"+mois.getSelectedIndex()+"-"+jour.getSelectedItem();
+			try {
+				dateJava = sdf.parse(dateText);
+			} catch (ParseException e) {
+				e.printStackTrace();
+			}
+			return dateJava;
+		}
+	}
+
