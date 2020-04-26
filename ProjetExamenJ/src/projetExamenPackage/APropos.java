cc57b4fc11f0d43712b3b1f4b428801b19293647
Maxou <Maxou@Pc-Bureau-Max>
2020-04-26 22:26:22 +0200

    update aPropos

--- ProjetExamenJ/src/projetExamenPackage/APropos.java
+++ ProjetExamenJ/src/projetExamenPackage/APropos.java
@@ -2,13 +2,15 @@ package projetExamenPackage;
 
 import java.awt.*;
 import java.awt.event.*;
+import java.net.*;
+
 import javax.swing.*;
 
 
 public class APropos extends JFrame {
-
 	private JLabel labelTexte1,labelTexte2,labelTexte3,labelTexte4;
-	private JButton boutonRetour;
+	private JButton boutonRetour, boutonMaxime,boutonPaul;
+	private Desktop bureau = Desktop.getDesktop();
 	
 	
 	public APropos() {
@@ -25,6 +27,19 @@ public class APropos extends JFrame {
         labelTexte2.setBounds(80, 40, 255, 45);
         add(labelTexte2);
         
+        //Bouton
+        boutonMaxime = new JButton("Maxime");     
+        boutonMaxime.setBounds(80,100,100,30);  
+        ActionMaxime actionMaxime = new ActionMaxime();     
+        boutonMaxime.addActionListener(actionMaxime); 
+        add(boutonMaxime);
+        
+        boutonPaul = new JButton("Paul");     
+        boutonPaul.setBounds(180,100,100,30);  
+        ActionPaul actionPaul = new ActionPaul();     
+        boutonPaul.addActionListener(actionPaul); 
+        add(boutonPaul);
+        
         //Texte 3
         labelTexte3 = new JLabel("Pour le cours Développement orienté objet avancé "); 
         labelTexte3.setBounds(80, 60, 300, 45);
@@ -34,6 +49,7 @@ public class APropos extends JFrame {
         labelTexte4 = new JLabel("  "); 
         labelTexte4.setBounds(80, 60, 255, 45);
         add(labelTexte4);
+        
         
         //Bouton retour
         boutonRetour = new JButton("Retour au menu principal");     
@@ -49,5 +65,27 @@ public class APropos extends JFrame {
 		public void actionPerformed(ActionEvent e) { 
 			dispose(); 
 		}
+	}
+	
+	private class ActionMaxime implements ActionListener {
+	  public void  actionPerformed(ActionEvent e)  {
+        try {
+            bureau.browse(new URI("https://github.com/LeigerMax"));
+            } 
+        catch (Exception o) {
+            o.printStackTrace();
+            }
+      }
+	}
+	
+	private class ActionPaul implements ActionListener {
+		  public void  actionPerformed(ActionEvent e)  {
+	        try {
+	            bureau.browse(new URI("https://github.com/PaulApg"));
+	            } 
+	        catch (Exception o) {
+	            o.printStackTrace();
+	            }
+	      }
 	}
 }
